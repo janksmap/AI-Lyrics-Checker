ab7a8e58dd178600c15c2dbc346a9e7b05d782cd
janksmap <64335090+janksmap@users.noreply.github.com>
2024-06-13 03:56:07 -0600

    Cors fix

--- app.js
+++ app.js
@@ -6,16 +6,15 @@ const port = 3000;
 
 // Middleware to parse JSON bodies
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
-// CORS middleware
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'chrome-extension://phlgeijgelnfglkjpplhlmjniadehokp');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-  });
+const corsOptions = {
+    origin: 'chrome-extension://phlgeijgelnfglkjpplhlmjniadehokp',
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+};
+
+app.use(cors(corsOptions));
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
